2c8618fde56a38832dcb60018a4666e6a30e336c
May8326 <maelys.gioan@outlook.fr>
2025-06-10 19:26:26 +0200

    changé uuid

--- scripts/translation_example.sh
+++ scripts/translation_example.sh
@@ -3,7 +3,7 @@ LANG=pl
 ENCODING=UTF-8
 LOCALE=pl_PL.utf8
 
-xgettext --from-code=$ENCODING --output=po/todoist@tarelda.github.com.pot *.js
-msginit --no-translator --input po/todoist@tarelda.github.com.pot --output-file=po/$LANG.po --locale=$LOCALE --no-wrap
+xgettext --from-code=$ENCODING --output=po/todoist-indicator@may8326.github.com.pot *.js
+msginit --no-translator --input po/todoist-indicator@may8326.github.com.pot --output-file=po/$LANG.po --locale=$LOCALE --no-wrap
 # apply translations to po file and after that generate final mo file
-msgfmt po/$LANG.po -o locale/$LANG/LC_MESSAGES/todoist@tarelda.github.com.mo
+msgfmt po/$LANG.po -o locale/$LANG/LC_MESSAGES/todoist-indicator@may8326.github.com.mo
